e9089d1c3de8203f14d7e5d00241303f524211d6
Brian <wuquderen@hotmail.com>
2026-09-20 10:32:32 +0800

    9.20 10：31  提交level2\p02_is_prime

---
+++ level1/p02_is_prime/main.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int n;
+    cin>>n;
+    bool flag = 0;
+    for (int i = 2; i * i <= n; i++)
+        if (n % i == 0) {
+            flag = 1;
+            break;
+        }
+    if (flag == 0)
+        cout << "YES" << endl;
+    else
+        cout << "NO" << endl;
+    system("pause");
+    return 0;
+}
